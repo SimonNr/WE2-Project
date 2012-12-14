b863cd42f9de38d488e05b469e7fe3d26c7cae3b
Simon <sm287415@fh-muenster.de>
2012-12-14 16:56:01 +0100

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WE2Project::Application.config.secret_token = '2cc4de3f86dbd58d5094a54b955f3be9c0094c89b37351a34e0c040e82f2213a9fbc1e33b03080aa79e491a8c0c5087df05a61d0cca3a6a4296ec8ee4a823d40'
